6eee21cfcb4cc7a4ad004ad257efe4fb4fba82aa
Roshan Adhikari <nahsorad@gmail.com>
2021-04-30 21:38:46 +0545

    Add setContent and setSynopsis methods for Post POJO

--- src/main/java/np/com/roshanadhikary/blog/entity/Post.java
+++ src/main/java/np/com/roshanadhikary/blog/entity/Post.java
@@ -14,14 +14,34 @@ public class Post {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column
 	private long id;
+
 	@Column
 	private String title;
-	@Column
+
+	@Column(length = 5000)
 	private String content;
+
+	@Column(length = 150)
+	private String synopsis;
+
 	@ManyToOne
 	@JoinColumn(name = "author_id")
 	private Author author;
+
 	@Column
 	@Convert(converter = LocalDateTimeConverter.class)
 	private LocalDateTime dateTime;
+
+	public void setContent(String markdownContent) {
+		MdToHtmlRenderer renderer = new MdToHtmlRenderer();
+
+		this.content = renderer.render(markdownContent);
+	}
+
+	public void setSynopsis(String markdownContent) {
+		MdToHtmlRenderer renderer = new MdToHtmlRenderer();
+		String content = renderer.render(markdownContent);
+
+		this.synopsis = content.length() <= 150 ? content : content.substring(0, 149);
+	}
 }
