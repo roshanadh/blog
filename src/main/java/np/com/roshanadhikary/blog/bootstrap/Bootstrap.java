125ea17b17d0e1c6643bd1be98b586e2cfacaec4
Roshan Adhikari <nahsorad@gmail.com>
2021-04-30 19:30:50 +0545

    Bootstrap Post and Author entities on ContextRefreshedEvent

---
+++ src/main/java/np/com/roshanadhikary/blog/bootstrap/Bootstrap.java
@@ -0,0 +1,53 @@
+package np.com.roshanadhikary.blog.bootstrap;
+
+import np.com.roshanadhikary.blog.entity.*;
+import np.com.roshanadhikary.blog.repository.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.context.*;
+import org.springframework.context.event.*;
+import org.springframework.stereotype.*;
+
+import java.time.*;
+import java.util.*;
+
+@Component
+public class Bootstrap implements ApplicationListener<ContextRefreshedEvent> {
+	@Autowired
+	private AuthorRepository authorRepository;
+	@Autowired
+	private PostRepository postRepository;
+
+	public Author bootstrapAuthor() {
+		Optional<Author> authorOptional = authorRepository.findById(1L);
+		if (authorOptional.isPresent()) {
+			return authorOptional.get();
+		} else {
+			Author roshanAuthor = new Author();
+			roshanAuthor.setName("Roshan Adhikari");
+			roshanAuthor.setEmail("nahsorad@gmail.com");
+			roshanAuthor.setUrl("roshanadhikari.name.np");
+
+			authorRepository.save(roshanAuthor);
+			return roshanAuthor;
+		}
+	}
+
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+		System.out.println("Bootstrapping posts...");
+
+		Optional<Post> postOptional = postRepository.findById(1L);
+		if (postOptional.isEmpty()) {
+			Post helloWorldPost = new Post();
+			String helloWorldPostContent = """
+			Hello World! *This is my first post*!
+			""";
+			helloWorldPost.setTitle("Hello World!");
+			helloWorldPost.setAuthor(bootstrapAuthor());
+			helloWorldPost.setContent(helloWorldPostContent);
+			helloWorldPost.setDateTime(LocalDateTime.now());
+
+			postRepository.save(helloWorldPost);
+		}
+	}
+}
